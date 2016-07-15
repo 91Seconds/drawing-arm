c318986d191693800df211d5e39259eb7a8ccc05
91Seconds <king.ciaran72@gmail.com>
2016-07-15 22:37:33 +1200

    seprated calcing elbow pos from drawing arms

--- full/src/main/java/Main.java
+++ full/src/main/java/Main.java
@@ -64,13 +64,52 @@ public class Main extends PApplet {
         ////targetX=xCoOrdCenter+160*sin((float)frameCount/16);
         //targetY=yCoOrdCenter+180*cos((float)frameCount/19);
 
+
+
         apparatus();
-        arms();
+        drawArms(findElbowPos(),-1,1);
         gCursor();
     }
 
-    void arms() {
-        rect(o1X,o1Y,10,10);
+    private void drawArms(float[] elbowPos, int leftConfig, int rightConfig) {
+        stroke(0xff,0xff,0xff,0x22);
+        line(elbowPos[0], elbowPos[1], elbowPos[2], elbowPos[3]);
+        line(elbowPos[4], elbowPos[5], elbowPos[6], elbowPos[7]);
+        ellipse((elbowPos[0] + elbowPos[2]) / 2, (elbowPos[1] + elbowPos[3]) / 2, absLength(elbowPos[0], elbowPos[2], elbowPos[1], elbowPos[3]), absLength(elbowPos[0], elbowPos[2], elbowPos[1], elbowPos[3]));
+        ellipse((elbowPos[4] + elbowPos[6]) / 2, (elbowPos[5] + elbowPos[7]) / 2, absLength(elbowPos[4], elbowPos[6], elbowPos[5], elbowPos[7]), absLength(elbowPos[4], elbowPos[6], elbowPos[5], elbowPos[7]));
+
+        if(leftConfig<=0) {
+            stroke(0xff,0x00,0x00,0x88);
+            line(targetX, targetY, elbowPos[0], elbowPos[1]);
+            stroke(0x00,0x00,0xff,0x88);
+            line(o1X,o1Y,elbowPos[0],elbowPos[1]);
+        }
+        if(rightConfig<=0) {
+            stroke(0xff,0x00,0x00,0x88);
+            line(targetX, targetY, elbowPos[4], elbowPos[5]);
+            stroke(0x00,0x00,0xff,0x88);
+            line(o2X,o2Y,elbowPos[4],elbowPos[5]);
+        }
+
+        if(leftConfig>=0) {
+            stroke(0xff,0x00,0x00,0xdd);
+            line(targetX, targetY, elbowPos[2], elbowPos[3]);
+            stroke(0x00,0x00,0xff,0xdd);
+            line(o1X,o1Y,elbowPos[2],elbowPos[3]);
+        }
+        if(rightConfig>=0) {
+            stroke(0xff,0x00,0x00,0xdd);
+            line(targetX, targetY, elbowPos[6], elbowPos[7]);
+            stroke(0x00,0x00,0xff,0xdd);
+            line(o2X,o2Y,elbowPos[6],elbowPos[7]);
+        }
+
+
+
+
+    }
+
+    float[] findElbowPos() {
         //co-ordinates of the center point of a line drawn from the shoulders to the mouse
         float o1XC = (targetX+o1X)/2;
         float o1YC = (targetY+o1Y)/2;
@@ -78,20 +117,15 @@ public class Main extends PApplet {
         float o2YC = (targetY+o2Y)/2;
 
         //length of the line between the shoulders and the mouse
-        float abs1 = sqrt(   pow(o1X-targetX,2)+pow(o1Y-targetY,2)   );
-        float abs2 = sqrt(   pow(o2X-targetX,2)+pow(o2Y-targetY,2)   );
+        float abs1 = absLength(o1X,targetX,o1Y,targetY);
+        float abs2 = absLength(o2X,targetX,o2Y,targetY);
 
         //radius of a circle centered halfway between the shoulders and the mouse so that the distance
         //between its intersection with the reach of the shoulders and the mouse is equal to l
-        float o1R = findRadius(l,abs1);
-        float o2R = findRadius(l,abs2); //(pow(((mouseX-o2X)/2),2)-pow(mouseY-o2Y,2)));
+        float o1R = findOp(l,abs1/2);
+        float o2R = findOp(l,abs2/2);
 
-        //draws that circle
-        stroke(80);
-        ellipse(o1XC,o1YC,2*o1R,2*o1R);
-        ellipse(o2XC,o2YC,2*o2R,2*o2R);
-
-        //finds the angle of the line fromn the shoulders to the mouse
+        //finds the angle of the line from the shoulders to the mouse
         //and the inverse reciprocal
         float o1Angle = atan2((targetY-o1Y),(targetX-o1X));
         float o1NormalAngle =atan2(-(targetX-o1X),(targetY-o1Y));
@@ -111,44 +145,16 @@ public class Main extends PApplet {
         float o2NormalX2 = o2XC - o2R*cos(o2NormalAngle);
         float o2NormalY2 = o2YC - o2R*sin(o2NormalAngle);
 
-        //draws the normals
-        stroke(60);
-        line(o1NormalX2,o1NormalY2,o1NormalX1,o1NormalY1);
-        line(o2NormalX2,o2NormalY2,o2NormalX1,o2NormalY1);
-
-        //draws the one possiblility for the ulnars
-        stroke(255,0,0);
-        line(o1X,o1Y,o1NormalX1,o1NormalY1);
-        line(o2X,o2Y,o2NormalX1,o2NormalY1);
-
-        //draws the other possiblility for the ulnars
-        stroke(150,0,0);
-        line(o1X,o1Y,o1NormalX2,o1NormalY2);
-        line(o2X,o2Y,o2NormalX2,o2NormalY2);
-
-        //draws one posssiblility for the foreamrs
-        stroke(0,0,255);
-        line(targetX,targetY,o1NormalX1,o1NormalY1);
-        line(targetX,targetY,o2NormalX1,o2NormalY1);
-
-        //draws the other possiblility for the forearms
-        stroke(0,0,150);
-        line(targetX,targetY,o1NormalX2,o1NormalY2);
-        line(targetX,targetY,o2NormalX2,o2NormalY2);
-
-
-
-
-
-
-        //draws a straight line from shoulder to mouse
-        stroke(100);
-        line(o1X,o1Y,targetX,targetY);
-        line(o2X,o2Y,targetX,targetY);
+        float[] points = {o1NormalX1,o1NormalY1,o1NormalX2,o1NormalY2,o2NormalX1,o2NormalY1,o2NormalX2,o2NormalY2};
+        return points;
     }
 
-    private float findRadius(float l, float length) {
-        return sqrt(pow(l,2)-pow(length/2,2));
+    private float absLength(float X1, float X2, float Y1, float Y2) {
+        return sqrt(   pow(X1-X2,2)+pow(Y1-Y2,2)   );
+    }
+
+    private float findOp(float hyp, float adj) {
+        return sqrt(pow(hyp,2)-pow(adj,2));
     }
 
     //draws the reach of the ulnars pivoting from shoulders and the total working
@@ -161,6 +167,9 @@ public class Main extends PApplet {
         noFill();
         ellipse(o1X,o1Y,4*l,4*l);
         ellipse(o2X,o2Y,4*l,4*l);
+        stroke(0xff,0xff,0xff,0x22);
+        ellipse(o1X,o1Y,2*l,2*l);
+        ellipse(o2X,o2Y,2*l,2*l);
     }
 
     //displays the mouse position
